66b1e256e4cc6339fccf8100e2b7b6a4da177f15
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-03-24 22:12:05 -0700

    update site data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Ember & Ash`,
+    author: `Jamie Pilgrim`,
+    description: `Hike Blog`,
+    siteUrl: `https://illbringthedonuts.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pilgrimmemoirs`,
     },
   },
   plugins: [
