f530181d2fcf6bab52403cc847cb24cf08d32b6a
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-04-15 22:58:35 -0700

    Move Header into it's own component

---
+++ src/components/header.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { Link } from "gatsby"
+
+
+class Header extends React.Component {
+  render() {
+    const rootPath = `${__PATH_PREFIX__}/`
+    const blogPath = `${__PATH_PREFIX__}/blog/`
+    const title = this.props.title
+    let header
+
+    if (location.pathname === rootPath || location.pathname === blogPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+            position: 'fixed',
+            top: 0,
+            left: 0
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={location.pathname === blogPath ? `/` : `/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+            margin: '1rem',
+            position: 'fixed',
+            top: 0,
+            left: 0
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/blog/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+
+    return (
+      <header>{header}</header>
+    )
+  }
+}
+
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,69 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "./header"
 import styled from "styled-components"
 import "./layout.css"
 
 import { rhythm, scale } from "../utils/typography"
 
+
+
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header
 
-    if (location.pathname === rootPath || location.pathname === blogPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-            position: 'fixed',
-            top: 0,
-            left: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={location.pathname === blogPath ? `/` : `/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            margin: '1rem',
-            position: 'fixed',
-            top: 0,
-            left: 0
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/blog/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
       <Wrapper>
-        <header>{header}</header>
+        <Header title={title}></Header>
         <div>
           <main>{children}</main>
         </div>
