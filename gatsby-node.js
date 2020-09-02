d8fa4bb0add78eb3560654415e24fe7ccc30ee43
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-01 23:03:27 -0700

    add blog post template and createPage for each posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,44 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(
+    'src/templates/blogPostTemplate.js'
+  );
+
+  return graphql(`
+    {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors;
+    }
+
+    const posts = result.data.allMdx.nodes;
+
+    // create page for each mdx file
+    posts.forEach(post => {
+      createPage({
+        path: post.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.fields.slug,
+        },
+      });
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
