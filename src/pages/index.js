11a4efdc59f6b0025c8393c304e03eb1bfa8a08f
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-01 21:10:53 -0700

    add graphql query to index for posts, show published posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { Layout } from '../components/Layout';
 
-export default () => {
+export default ({ data }) => {
   return (
     <>
-      <Layout />
+      <Layout >
+        {data.allMdx.nodes.map(({ excerpt, frontmatter }) => (
+          <>
+            <h1>{frontmatter.title}</h1>
+            <p>{frontmatter.date}</p>
+            <p>{excerpt}</p>
+          </>
+        ))}
+      </Layout>
     </>
   )
 }
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`;
