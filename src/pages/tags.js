73f6fbe1fc89715c4c8921cfe108352fd4d3bce9
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-13 18:39:13 -0700

    /tags page shows list of tags - no styling

---
+++ src/pages/tags.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link, graphql } from "gatsby";
+
+export default ({
+  data: { allMdx: { group }, },
+}) => {
+  return (
+    <div>
+      <div>
+        <h1>Tags</h1>
+        <ul>
+          {group.map(tag => (
+            <li key={tag.fieldValue}>
+              <Link to={`/tags/${tag.fieldValue}/`}>
+                {tag.fieldValue} ({tag.totalCount})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query {
+    allMdx(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
--- src/templates/tagTemplate.js
+++ src/templates/tagTemplate.js
@@ -1,59 +1,37 @@
-import React from "react"
-import PropTypes from "prop-types"
-// Components
-import { Link, graphql } from "gatsby"
-const Tags = ({ pageContext, data }) => {
-const { tag } = pageContext
-const { edges, totalCount } = data.allMarkdownRemark
-const tagHeader = `${totalCount} post${
-  totalCount === 1 ? "" : "s"
-} tagged with "${tag}"`
-return (
-  <div>
-    <h1>{tagHeader}</h1>
-    <ul>
-      {edges.map(({ node }) => {
-        const { slug } = node.fields
-        const { title } = node.frontmatter
-        return (
-          <li key={slug}>
-            <Link to={slug}>{title}</Link>
-          </li>
-        )
-      })}
-    </ul>
-    {/*
-            This links to a page that does not yet exist.
-            You'll come back to it!
-          */}
-    <Link to="/tags">All tags</Link>
-  </div>
-)
+import React from "react";
+import { Link, graphql } from "gatsby";
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import SEO from 'react-seo-component';
+import { Layout } from '../components/Layout';
+import Img from "gatsby-image"
+
+export default ({ data, pageContext }) => {
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMdx;
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } tagged with "${tag}"`
+  return (
+    <div>
+      <h1>{tagHeader}</h1>
+      <ul>
+        {edges.map(({ node }) => {
+          const { slug } = node.fields
+          const { title } = node.frontmatter
+          return (
+            <li key={slug}>
+              <Link to={slug}>{title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/tags">All tags</Link>
+    </div>
+  )
 }
-Tags.propTypes = {
-pageContext: PropTypes.shape({
-  tag: PropTypes.string.isRequired,
-}),
-data: PropTypes.shape({
-  allMarkdownRemark: PropTypes.shape({
-    totalCount: PropTypes.number.isRequired,
-    edges: PropTypes.arrayOf(
-      PropTypes.shape({
-        node: PropTypes.shape({
-          frontmatter: PropTypes.shape({
-            title: PropTypes.string.isRequired,
-          }),
-          fields: PropTypes.shape({
-            slug: PropTypes.string.isRequired,
-          }),
-        }),
-      }).isRequired
-    ),
-  }),
-}),
-}
-export default Tags
-export const pageQuery = graphql`
+
+
+export const query = graphql`
 query($tag: String) {
   allMdx(
     limit: 2000
