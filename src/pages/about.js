2c2f411a835fa81f6876f7ae5bffe1a8924583e0
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-15 16:48:12 -0700

    add socials to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,5 +16,15 @@ export default () => (
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </div>
+    <hr />
+    <div className={aboutStyles.socials}>
+      <h4>Follow On:</h4>
+      <ul>
+        <li><a href="https://www.instagram.com/pilgrimmemoirs/" target="_blank">Instagram</a></li>
+        <li><a href="https://www.twitter.com/pilgrimmemoirs/" target="_blank">Twitter</a></li>
+        <li><a href="https://unsplash.com/@pilgrimmemoirs" target="_blank">Unsplash</a></li>
+        <li><a href="https://society6.com/pilgrimmemoirs" target="_blank">Society 6</a></li>
+      </ul>
+    </div>
   </Layout>
 )
