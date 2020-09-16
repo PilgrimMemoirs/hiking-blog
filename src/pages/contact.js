8e261a8f8b92d00d23e9863454e8db8a71839862
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-15 18:00:08 -0700

    test netlify contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,5 +5,20 @@ import { Layout } from '../components/Layout';
 export default () => (
   <Layout>
     <h2> Contact </h2>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
   </Layout>
 )
