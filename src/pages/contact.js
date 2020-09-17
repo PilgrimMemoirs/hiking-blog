89ad8b640e545e3014e0055ee6430618021ada5e
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-16 22:13:36 -0700

    basic contact page styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, graphql } from "gatsby";
 import { Layout } from '../components/Layout';
 import { navigate } from 'gatsby-link'
+import contactStyles from '../components/contact.module.scss'
+import image from '../../content/imgs/jamie-pilgrim-2.jpg'
 
 function encode(data) {
   return Object.keys(data)
@@ -33,28 +35,43 @@ export default () => {
 
   return (
     <Layout>
-      <h2> Contact </h2>
-      <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <img src={image} className={contactStyles.contactImg}/>
 
+      <div class={contactStyles.form}>
+        <h2> Contact </h2>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+
+      <hr />
+
+      <div className={contactStyles.socials}>
+        <h4>Follow On:</h4>
+        <ul>
+          <li><a href="https://www.instagram.com/pilgrimmemoirs/" target="_blank">Instagram</a></li>
+          <li><a href="https://www.twitter.com/pilgrimmemoirs/" target="_blank">Twitter</a></li>
+          <li><a href="https://unsplash.com/@pilgrimmemoirs" target="_blank">Unsplash</a></li>
+          <li><a href="https://society6.com/pilgrimmemoirs" target="_blank">Society 6</a></li>
+        </ul>
+      </div>
     </Layout>
   )
 }
