fbd0ac7b583904ff88655d3144113f645d48da6d
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-26 21:20:43 -0700

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,6 @@ export const Footer = () => (
   <footer className="site-footer">
     <h3><Link to="/">Memoirs of a Pilgrim</Link></h3>
     <p>&#169; 2020 Jamie Pilgrim</p>
-    <p>Content may not be used without permission</p>
+    <p>Content may not be used without permission.</p>
   </footer>
 );
