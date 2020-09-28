ded0c40d15e1975a370fd73aa2ae67ab40a55655
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-09-27 19:04:04 -0700

    update about styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link, graphql } from "gatsby";
 import { Layout } from '../components/Layout/Layout';
 
-import aboutStyles from '../styles/about.scss';
+import '../styles/about.scss';
 import image from '../../content/imgs/jamie-pilgrim-2.jpg';
 
 export default () => (
   <Layout>
-    <img src={image} className={aboutStyles.aboutImg}/>
-    <div className={aboutStyles.bodyText}>
+    <img src={image} className="about-img"/>
+    <div className="body-text">
       <h2> About </h2>
       <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -18,7 +18,7 @@ export default () => (
       </p>
     </div>
     <hr />
-    <div className={aboutStyles.socials}>
+    <div className="socials">
       <h4>Follow On:</h4>
       <ul>
         <li><a href="https://www.instagram.com/pilgrimmemoirs/" target="_blank">Instagram</a></li>
