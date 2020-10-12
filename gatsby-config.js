d22b5560c499e108c01211f038b23db1e590c623
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2020-10-12 00:45:03 -0700

    update last two blogs posts with youtube embeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,6 @@ const siteMetadata = {
 module.exports = {
   siteMetadata: siteMetadata,
   plugins: [
-    `gatsby-remark-embed-video`,
-    `gatsby-remark-responsive-iframe`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
@@ -30,34 +28,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: { path: `${__dirname}/content/posts`, name: `posts` },
-    },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-        {
-          resolve: "gatsby-remark-embed-video",
-          options: {
-            width: 800,
-            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-            height: 400, // Optional: Overrides optional.ratio
-            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-            noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-            urlOverrides: [
-              {
-                id: 'youtube',
-                embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
-              }
-            ],
-            containerClass: 'embedVideo-container',
-          }
-        }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
@@ -70,6 +40,10 @@ module.exports = {
           },
         ],
       },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: { path: `${__dirname}/content/posts`, name: `posts` },
     },
   ],
 };
